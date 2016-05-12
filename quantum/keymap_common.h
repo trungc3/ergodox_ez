456f6eb78e85cd67b4c8ad7d479b47953a527008
trung <trungpham@asnet.com.vn>
2016-05-12 09:16:02 +0700

    add control + alt key

--- quantum/keymap_common.h
+++ quantum/keymap_common.h
@@ -245,6 +245,7 @@ extern const uint16_t fn_actions[];
 #define ALT_T(kc) MT(0x4, kc)
 #define GUI_T(kc) MT(0x8, kc)
 #define C_S_T(kc) MT(0x3, kc) // Control + Shift e.g. for gnome-terminal
+#define C_A_T(kc) MT(0x5, kc) // control + alt
 #define MEH_T(kc) MT(0x7, kc) // Meh is a less hyper version of the Hyper key -- doesn't include Win or Cmd, so just alt+shift+ctrl
 #define LCAG_T(kc) MT(0xD, kc) // Left control alt and gui
 #define ALL_T(kc) MT(0xF, kc) // see http://brettterpstra.com/2012/12/08/a-useful-caps-lock-key/
